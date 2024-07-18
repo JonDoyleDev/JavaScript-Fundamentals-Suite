ee45e67b108a8fecef9458a22d5656f8bbf031d3
crmdspx <35460998+crmdspx@users.noreply.github.com>
2024-07-18 15:09:31 -0600

    updated weather app

---
+++ components/weather.js
@@ -0,0 +1,7 @@
+const time = document.getElementById('time');
+const date = document.getElementById('date');
+const currentWeatherItems = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const currentTemp = document.getElementById("current-temp");
+const location = document.getElementById("location");
+const weatherForecast = document.getElementById('weather-forecast');
